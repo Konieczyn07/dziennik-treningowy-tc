01dacd71a14addadfb532ea5ecadbad8f62b0be1
Michał Konieczny <michalkonieczny2005@gmail.com>
2026-06-15 19:40:42 +0200

    Sql server + env

--- api/config/database.php
+++ api/config/database.php
@@ -1,17 +1,26 @@
 <?php
 
 class Database {
-	public $username = getenv('DATABASE_USERNAME');
-	public $password = getenv('DATABASE_PASSWORD');
+	public $username = getenv("DATABASE_USERNAME");
+	public $password = getenv("DATABASE_PASSWORD");
 	
 	public function getConn() {
-		
+		try{
+			$connectionInfo = array("UID" => $this->$username, "pwd" => $this->$password, "Database" => "dzienniktreningowy-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+			$serverName = "tcp:dzienniktreningowy-sqldb.database.windows.net,1433";
+			$conn = sqlsrv_connect($serverName, $connectionInfo);
 
-		$connectionInfo = array("UID" => $this->$username, "pwd" => $this->$password, "Database" => "dzienniktreningowy-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-		$serverName = "tcp:dzienniktreningowy-sqldb.database.windows.net,1433";
-		$conn = sqlsrv_connect($serverName, $connectionInfo);
+			return $conn;
+		}catch{
+			$this->username = "rootadmin";
+			$this->password = "=fX^anHD~W4a2#.D3ZP1";
 
-		return $conn;
+			$connectionInfo = array("UID" => $this->$username, "pwd" => $this->$password, "Database" => "dzienniktreningowy-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+			$serverName = "tcp:dzienniktreningowy-sqldb.database.windows.net,1433";
+			$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+			return $conn;
+		}
 	}
 }
 
