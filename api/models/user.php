b6c93fb7240ea0eca76bba9014edc0a310eee844
Michał Konieczny <michalkonieczny2005@gmail.com>
2026-06-15 22:11:21 +0200

    Sql server code changes

--- api/models/user.php
+++ api/models/user.php
@@ -44,7 +44,7 @@ class User {
         $this->username = htmlspecialchars(strip_tags($this->username));
         $params = array($this->username);
         $stmt = sqlsrv_query($this->conn, $query, $params);
-        row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC);
+        $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC);
         
         if($row && password_verify($this->password, $row['password'])) {
             $this->id = $row['id'];
